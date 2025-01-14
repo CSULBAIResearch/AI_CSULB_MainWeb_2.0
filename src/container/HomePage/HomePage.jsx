8260570879a98b8e2d94bd36e1a8019da246b4d8
KinokoLu <myluwork004@gmail.com>
2025-01-14 04:54:52 -0800

    minor fix, empty h1 homepage

--- src/container/HomePage/HomePage.jsx
+++ src/container/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <section id = "hero">
         <div className="hero box__container flex__center">
             <div>
-                <h1></h1>
+                
                 <h1 className='headtext__montserrat'>AI RESEARCH CLUB <span></span></h1>
                 <h1 className = "school-name p__montserrat">CALIFORNIA STATE UNIVERSITY, LONG BEACH <span></span></h1>
             </div>
